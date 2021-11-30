002dc65a063b97937f7eb13a55d27c44144634f9
Manuela Quertier <manuelaquertier@gmail.com>
2021-11-30 17:31:09 +0100

    quest10

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -10,6 +12,72 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Category
 {
+    /**
+     * @ORM\OneToMany(targetEntity=Program::class, mappedBy="category")
+     */
+
+    private $programs;
+
+    public function __construct()
+
+    {
+
+        $this->programs = new ArrayCollection();
+    }
+    /**
+     * @return Collection|Program[]
+     */
+
+    public function getPrograms(): Collection
+
+    {
+
+        return $this->programs;
+    }
+    /**
+
+     * @param Program $program
+
+     * @return Category
+
+     */
+
+    public function addProgram(Program $program): self
+
+    {
+
+        if (!$this->programs->contains($program)) {
+
+            $this->programs[] = $program;
+
+            $program->setCategory($this);
+        }
+
+
+        return $this;
+    }
+
+    /**
+     * @param Program $program
+     * @return Category
+     */
+
+    public function removeProgram(Program $program): self
+
+    {
+
+        if ($this->programs->removeElement($program)) {
+
+            // set the owning side to null (unless already changed)
+
+            if ($program->getCategory() === $this) {
+
+                $program->setCategory(null);
+            }
+        }
+        return $this;
+    }
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
