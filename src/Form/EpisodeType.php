675bfadd18241ee504f776c059928053812c2da9
Manuela Quertier <manuelaquertier@gmail.com>
2021-12-01 11:50:46 +0100

    quest13 CRUD

--- src/Form/EpisodeType.php
+++ src/Form/EpisodeType.php
@@ -2,32 +2,31 @@
 
 namespace App\Form;
 
-use App\Entity\Program;
+use App\Entity\Episode;
+
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProgramType extends AbstractType
+class EpisodeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('title', TextType::class)
-            ->add('summary', TextareaType::class)
-            ->add('poster', TextType::class)
-            ->add('country', TextType::class)
-            ->add('year', IntegerType::class)
-            ->add('Category', null, ['choice_label' => 'name'])
+            ->add('number', IntegerType::class)
+            ->add('synopsis', TextareaType::class)
+            ->add('season', null, ['choice_label' => 'number'])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Program::class,
+            'data_class' => Episode::class,
         ]);
     }
 }
